bf4aa9ccba2f96fea7b1001c8c08a466fc1217f1
arna <alireza.real98@gmail.com>
2023-03-29 14:59:32 +0430

    add ThemeData and set fontFamily

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(HomePage());
 }
 
-class HomePage extends StatelessWidget{
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa'), // Farsi
+      ],
+      theme: ThemeData(
+        fontFamily: 'nazanin'
+      ),
     );
   }
-
 }

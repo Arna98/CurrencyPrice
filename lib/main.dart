ebeff70ad39f8411156c5018a727f3e7012963ff
arna <alireza.real98@gmail.com>
2023-03-29 13:30:05 +0430

    create HomePage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  
+  runApp(HomePage());
 }
 
+class HomePage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}

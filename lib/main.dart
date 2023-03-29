18469718e61e0caaee768b38bd08726f9bcd4059
arna <alireza.real98@gmail.com>
2023-03-29 16:34:26 +0430

    adding scaffold and create appbar

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,25 @@ class HomePage extends StatelessWidget {
       supportedLocales: const [
         Locale('fa'), // Farsi
       ],
-      theme: ThemeData(
-        fontFamily: 'nazanin'
-      ),
+      theme: ThemeData(fontFamily: 'nazanin'),
+      home: Scaffold(
+          appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: [
+          Image.asset("assets/images/icon.png"),
+          const Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("قیمت به روز ارز",
+                      style: TextStyle(color: Colors.black)))),
+          Expanded(
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset("assets/images/menu.png"))),
+          const SizedBox(width: 8)
+        ],
+      )),
     );
   }
 }

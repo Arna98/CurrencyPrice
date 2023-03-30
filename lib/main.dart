0015320a4aba26280e17ef6442ac95c64b26d914
arna <alireza.real98@gmail.com>
2023-03-30 14:26:53 +0330

    edit ThemeData -> textStyles and set style for textViews

--- lib/main.dart
+++ lib/main.dart
@@ -23,72 +23,86 @@ class HomePage extends StatelessWidget {
           appBarTheme: const AppBarTheme(elevation: 0),
           fontFamily: 'nazanin',
           textTheme: const TextTheme(
-              headlineLarge: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'nazanin',
-                  fontWeight: FontWeight.w700),
               headlineSmall: TextStyle(
                   color: Colors.black,
                   fontFamily: 'nazanin',
                   fontWeight: FontWeight.w700),
-              titleSmall: TextStyle(
-                  color: Colors.white,
+              titleLarge: TextStyle(
+                  color: Colors.black,
                   fontFamily: 'nazanin',
-                  fontWeight: FontWeight.w300),
-              bodyMedium: TextStyle(
+                  fontWeight: FontWeight.w700),
+              titleMedium: TextStyle(
+                color: Colors.white,
+                fontFamily: 'nazanin',
+                fontWeight: FontWeight.w300
+              ),
+              bodyLarge: TextStyle(
                   color: Colors.black,
                   fontFamily: 'nazanin',
                   fontWeight: FontWeight.w300))),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          actions: [
-            Image.asset("assets/images/icon.png"),
-            const Padding(
-                padding: EdgeInsets.only(right: 8),
-                child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Text("قیمت به روز ارز",
-                        style: TextStyle(color: Colors.black)))),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Image.asset("assets/images/menu.png"))),
-            const SizedBox(width: 8)
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(children: [
-            Row(
-              children: [
-                Image.asset("assets/images/question.png"),
-                const SizedBox(width: 8),
-                const Text("نرخ ازاد ارز چیست؟")
+      home: const BodyWidget(),
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  const BodyWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: [
+          Image.asset("assets/images/icon.png"),
+           Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("قیمت به روز ارز",
+                      style: Theme.of(context).textTheme.headlineSmall))),
+          Expanded(
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset("assets/images/menu.png"))),
+          const SizedBox(width: 8)
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [
+          Row(
+            children: [
+              Image.asset("assets/images/question.png"),
+              const SizedBox(width: 8),
+              Text("نرخ ازاد ارز چیست؟", style: Theme.of(context).textTheme.titleLarge)
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 8, 0, 16),
+            child: Text(
+                "نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.",
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+          ),
+          Container(
+            height: 40,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 102, 102, 102),
+                borderRadius: BorderRadius.all(Radius.circular(1000))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children:[
+                Text("نام ارز", style: Theme.of(context).textTheme.titleMedium),
+                Text("قیمت", style: Theme.of(context).textTheme.titleMedium),
+                Text("تغییرات", style: Theme.of(context).textTheme.titleMedium),
               ],
             ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 8, 0, 16),
-              child: Text(
-                  "نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند."),
-            ),
-            Container(
-              height: 40,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 102, 102, 102),
-                  borderRadius: BorderRadius.all(Radius.circular(1000))),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  Text("ارز", style: TextStyle(color: Colors.white)),
-                  Text("قیمت", style: TextStyle(color: Colors.white)),
-                  Text("تغییرات", style: TextStyle(color: Colors.white)),
-                ],
-              ),
-            ),
-          ]),
-        ),
+          ),
+        ]),
       ),
     );
   }

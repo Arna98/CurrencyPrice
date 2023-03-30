aa0497994e1919ae1f6e8a14896519fc97b90f79
arna <alireza.real98@gmail.com>
2023-03-30 13:40:39 +0330

    implement the ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,46 @@ class HomePage extends StatelessWidget {
       supportedLocales: const [
         Locale('fa'), // Farsi
       ],
-      theme: ThemeData(fontFamily: 'nazanin'),
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 243, 243, 243),
+          appBarTheme: const AppBarTheme(elevation: 0),
+          fontFamily: 'nazanin',
+          textTheme: const TextTheme(
+              headlineLarge: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'nazanin',
+                  fontWeight: FontWeight.w700),
+              headlineSmall: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'nazanin',
+                  fontWeight: FontWeight.w700),
+              titleSmall: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'nazanin',
+                  fontWeight: FontWeight.w300),
+              bodyMedium: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'nazanin',
+                  fontWeight: FontWeight.w300))),
       home: Scaffold(
-          appBar: AppBar(
-        backgroundColor: Colors.white,
-        actions: [
-          Image.asset("assets/images/icon.png"),
-          const Padding(
-              padding: EdgeInsets.only(right: 8),
-              child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Text("قیمت به روز ارز",
-                      style: TextStyle(color: Colors.black)))),
-          Expanded(
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Image.asset("assets/images/menu.png"))),
-          const SizedBox(width: 8)
-        ],
-      )),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          actions: [
+            Image.asset("assets/images/icon.png"),
+            const Padding(
+                padding: EdgeInsets.only(right: 8),
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text("قیمت به روز ارز",
+                        style: TextStyle(color: Colors.black)))),
+            Expanded(
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Image.asset("assets/images/menu.png"))),
+            const SizedBox(width: 8)
+          ],
+        ),
+      ),
     );
   }
 }

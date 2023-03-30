4786559b4f759b702d7ef541e03a2930e51e4cb4
arna <alireza.real98@gmail.com>
2023-03-30 15:18:29 +0330

    implement listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,9 @@ class HomePage extends StatelessWidget {
                   fontFamily: 'nazanin',
                   fontWeight: FontWeight.w700),
               titleMedium: TextStyle(
-                color: Colors.white,
-                fontFamily: 'nazanin',
-                fontWeight: FontWeight.w300
-              ),
+                  color: Colors.white,
+                  fontFamily: 'nazanin',
+                  fontWeight: FontWeight.w300),
               bodyLarge: TextStyle(
                   color: Colors.black,
                   fontFamily: 'nazanin',
@@ -57,7 +56,7 @@ class BodyWidget extends StatelessWidget {
         backgroundColor: Colors.white,
         actions: [
           Image.asset("assets/images/icon.png"),
-           Padding(
+          Padding(
               padding: EdgeInsets.only(right: 8),
               child: Align(
                   alignment: Alignment.centerRight,
@@ -77,15 +76,16 @@ class BodyWidget extends StatelessWidget {
             children: [
               Image.asset("assets/images/question.png"),
               const SizedBox(width: 8),
-              Text("نرخ ازاد ارز چیست؟", style: Theme.of(context).textTheme.titleLarge)
+              Text("نرخ ازاد ارز چیست؟",
+                  style: Theme.of(context).textTheme.titleLarge)
             ],
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 8, 0, 16),
             child: Text(
-                "نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.",
-                style: Theme.of(context).textTheme.bodyLarge,
-                ),
+              "نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
           ),
           Container(
             height: 40,
@@ -95,14 +95,64 @@ class BodyWidget extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(1000))),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:[
+              children: [
                 Text("نام ارز", style: Theme.of(context).textTheme.titleMedium),
                 Text("قیمت", style: Theme.of(context).textTheme.titleMedium),
                 Text("تغییرات", style: Theme.of(context).textTheme.titleMedium),
               ],
             ),
           ),
+          //listview
+          SizedBox(
+            height: 350,
+            width: double.infinity,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int position) {
+                  return ListviewItem(context: context, position: position);
+                }),
+          )
         ]),
+      ),
+    );
+  }
+}
+
+class ListviewItem extends StatelessWidget {
+  late BuildContext _context;
+  late int _position;
+
+  ListviewItem({required BuildContext context, required int position, super.key}){
+    this._context = context;
+    this._position = position;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(1000)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              blurRadius: 1.0,
+              color: Colors.grey
+            )
+          ]
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text("دلار", style: Theme.of(context).textTheme.bodyLarge),
+            Text("46000R", style: Theme.of(context).textTheme.bodyLarge),
+            Text("+200", style: Theme.of(context).textTheme.bodyLarge)
+          ],
+        ),
       ),
     );
   }

d95fe30432a94cd1971cf893d9af0f77928896d3
arna <alireza.real98@gmail.com>
2023-04-01 14:17:29 +0330

    edit getter method in the currency class

--- lib/DataModels/CurrencyDataModel.dart
+++ lib/DataModels/CurrencyDataModel.dart
@@ -7,31 +7,21 @@ class CurrencyDataModel{
   
   set setId(String? id) => _id = id;
 
-  String? get getId{
-    return _id;
-  }
+  String? get getId => _id;
   
   set setTitle(String? title ) => _title = title;
 
-  String? get getTitle{
-    return _title;
-  }
+  String? get getTitle => _title;
 
   set setPrice(String? price) => _price = price;
 
-  String? get getPrice{
-    return _price;
-  }
+  String? get getPrice => _price;
 
   set setChanges(String? changes) => _changes = changes;
 
-  String? get getChanges{
-    return _changes;
-  }
+  String? get getChanges => _changes;
 
   set setStatus(String? status) => _status = status;
 
-  String? get getStatus{
-    return _status;
-  }
+  String? get getStatus => _status;
 }
